78ef6104e70905125692d83ca485e5c6a533e44d
Shafiqul Bari <120185849+shafiqulbari@users.noreply.github.com>
2023-09-02 17:29:34 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function textChange(){
+
+document.getElementById('h2').innerHTML="Shall we?";
+
+}
+
+function changeText(){
+    document.getElementById('tada').innerHTML="tadaaaa!!!!";
+}
+
+function transformText(){
+    document.getElementById('card-text').innerHTML="and here we go";
+}
